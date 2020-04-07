6b50bbf62cd980f4a3a877c6a53dd7efcf8ebd46
Paresh Borkar <paresh@threatwatch.io>
2020-04-07 12:51:00 +0000

    Bump version number

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/threatwatch/twigs',
-    version='1.0.29',
+    version='1.0.30',
     zip_safe=False,
     entry_points={
         'console_scripts': [
