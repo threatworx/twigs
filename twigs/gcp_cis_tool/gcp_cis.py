48d999577f54e8058c7478ac0cb02efd5918dc1c
Ketan Nilangekar <ketonne@yahoo.com>
2021-06-23 05:50:48 +0000

    Support for running CIS benchmarks for specific projects for GCP

--- twigs/gcp_cis_tool/gcp_cis.py
+++ twigs/gcp_cis_tool/gcp_cis.py
@@ -1,6 +1,7 @@
 import sys
 import subprocess
 import os
+import logging
 
 from . import check1 as check1
 from . import check2 as check2
@@ -14,6 +15,23 @@ from . import gcp_cis_utils as gcp_cis_utils
 def run_tests(args):
     gcp_cis_utils.set_encoding(args.encoding)
     config_issues = []
+    p_not_found = []
+    if args.projects:
+        allprojs = gcp_cis_utils.get_all_projects()
+        projs = args.projects.split(',')
+        gcp_cis_utils._projects = []
+        for p in projs:
+            if p in allprojs:
+                gcp_cis_utils._projects.append(p)
+            else:
+                p_not_found.append(p)
+
+    if len(p_not_found) > 0:
+        logging.error("Following project IDs were not found")
+        logging.error(p_not_found)
+        logging.error("Please provide correct project IDs and rerun")
+        sys.exit(1)
+
     config_issues.extend(check1.run_checks())
     config_issues.extend(check2.run_checks())
     config_issues.extend(check3.run_checks())
