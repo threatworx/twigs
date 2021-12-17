796151f77822c18688b78ec047d8c046fba1ebf7
Paresh Borkar <paresh@threatwatch.io>
2021-12-17 12:16:22 +0000

    Add debug for GCP CIS log metrics filter and alert tests

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/threatwatch/twigs',
-    version='1.0.94',
+    version='1.0.95',
     zip_safe=False,
     entry_points={
         'console_scripts': [
