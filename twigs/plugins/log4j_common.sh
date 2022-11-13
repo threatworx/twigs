8d47f51790af18d635ddc1a6509da1ecd79e650e
Paresh Borkar <pareshborkar@gmail.com>
2022-11-13 14:09:04 +0000

    Add support to detect log4j vulns in embedded log4j jar in jar/war files

--- twigs/plugins/log4j_common.sh
+++ twigs/plugins/log4j_common.sh
@@ -1,5 +1,39 @@
 #!/bin/sh
 
+check_embedded_log4j_vuln()
+{
+	ROOT_FOLDER="$1"
+	shift
+	CVE="$1"
+	shift
+	WAR_JAR_PATTERN="$1"
+	shift
+	VULN_VERSIONS="$*"
+	WAR_JAR_FILES=`find $ROOT_FOLDER -name "$WAR_JAR_PATTERN" -type f`
+	for WAR_JAR_FILE in $WAR_JAR_FILES
+	do
+		LOG4J_FILES=`jar -tf $WAR_JAR_FILE | grep 'log4j-core-.*\.jar'`
+		for LOG4J_FILE in $LOG4J_FILES
+		do
+			BASENAME=`basename $LOG4J_FILE`
+			LOG4J_VERSION=`echo $BASENAME | cut -c 12- | sed 's/.jar$//'`
+			for VULN_VERSION in $VULN_VERSIONS
+			do
+				if [ $LOG4J_VERSION = $VULN_VERSION ]; then
+					echo "TYPE:IMPACT"
+					echo "VULN:$CVE"
+					echo "PERCENTAGE:100"
+					echo "AFFECTED_PRODUCT:$WAR_JAR_FILE"
+					echo "VULNERABLE_PRODUCT:$BASENAME"
+					echo "ANALYSIS:Log4J Jar found in [$WAR_JAR_FILE] is vulnerable to $CVE"
+					echo "RECOMMENDATION:Upgrade to latest version of Log4J Jar"
+					echo ""
+				fi
+			done
+		done
+	done
+}
+
 check_log4j_vuln()
 {
 	ROOT_FOLDER="$1"
@@ -13,7 +47,7 @@ check_log4j_vuln()
 		for VULN_VERSION in $VULN_VERSIONS
 		do
 			BASENAME=`basename $LOG4J_FILE`
-			LOG4J_VERSION=`echo $BASENAME | cut -c 12- | cut -c -6`
+			LOG4J_VERSION=`echo $BASENAME | cut -c 12- | sed 's/.jar$//'`
 			if [ $LOG4J_VERSION = $VULN_VERSION ]; then
 				echo "TYPE:IMPACT"
 				echo "VULN:$CVE"
@@ -26,5 +60,7 @@ check_log4j_vuln()
 			fi
 		done
 	done
+	check_embedded_log4j_vuln $ROOT_FOLDER $CVE "*.war" $VULN_VERSIONS
+	check_embedded_log4j_vuln $ROOT_FOLDER $CVE "*.jar" $VULN_VERSIONS
 }
 
