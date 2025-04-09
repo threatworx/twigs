81be66f7e7901caea480f02edd2cee7ee982be27
Ketan Nilangekar <ketonne@yahoo.com>
2025-04-09 23:31:11 +0000

    New build

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/threatworx/twigs',
-    version='1.2.21',
+    version='1.2.22',
     zip_safe=False,
     entry_points={
         'console_scripts': [
