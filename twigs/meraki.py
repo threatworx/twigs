8ced15b18d033d72afeb80de1981ab9fd29584b1
Ketan Nilangekar <ketonne@yahoo.com>
2025-07-23 17:33:54 +0000

    Needed f-strings for urls

--- twigs/meraki.py
+++ twigs/meraki.py
@@ -10,25 +10,25 @@ import time
 from . import utils
 
 def get_organizations(args, headers):
-   url = args.base_url + '/organizations'
+   url = args.base_url + f'/organizations'
    resp = requests.get(url, headers=headers)
    resp.raise_for_status()
    return resp.json()
 
 def get_networks(args, headers, org_id):
-   url = args.base_url + '/organizations/{org_id}/networks'
+   url = args.base_url + f'/organizations/{org_id}/networks'
    resp = requests.get(url, headers=headers, verify=False)
    resp.raise_for_status()
    return resp.json()
 
 def get_devices(args, headers, org_id):
-   url = args.base_url + '/organizations/{org_id}/devices'
+   url = args.base_url + f'/organizations/{org_id}/devices'
    resp = requests.get(url, headers=headers, verify=False)
    resp.raise_for_status()
    return resp.json()
 
 def get_firmware_upgrades(args, headers, network_id):
-   url = args.base_url + '/networks/{network_id}/firmwareUpgrades'
+   url = args.base_url + f'/networks/{network_id}/firmwareUpgrades'
    resp = requests.get(url, headers=headers, verify=False)
    if resp.status_code == 404:
        return None  # Some networks may not support this endpoint
@@ -40,14 +40,14 @@ def get_all_devices(args, headers):
     logging.info("Retrieving network inventory from Cisco Meraki") 
     organizations = get_organizations(args, headers)
     for org in organizations:
-        logging.info("Organization: {org['name']} ({org['id']})")
+        logging.info(f"Organization: {org['name']} ({org['id']})")
         networks = get_networks(args, headers, org['id'])
         devices = get_devices(args, headers, org['id'])
         devices_by_network = {}
         for device in devices:
             devices_by_network.setdefault(device['networkId'], []).append(device)
         for net in networks:
-            logging.info("Network: {net['name']} ({net['id']})")
+            logging.info(f"Network: {net['name']} ({net['id']})")
             fw_info = get_firmware_upgrades(net['id'])
             fw_versions = {}
             if fw_info and 'products' in fw_info:
@@ -56,7 +56,7 @@ def get_all_devices(args, headers):
             for device in devices_by_network.get(net['id'], []):
                 product_type = device.get('productType', 'unknown')
                 fw_version = fw_versions.get(product_type, 'unknown')
-                logging.info("Device: {device.get('name', device['serial'])} | Model: {device['model']} | Product: {product_type} | Firmware: {fw_version}")
+                logging.info(f"Device: {device.get('name', device['serial'])} | Model: {device['model']} | Product: {product_type} | Firmware: {fw_version}")
                 asset = {}
                 asset['owner'] = args.handle
                 asset['id'] = device['serial']
