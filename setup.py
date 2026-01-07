d938fed8973b80452d7674f9be357e5d2efbdae5
Paresh Borkar <pareshborkar@gmail.com>
2026-01-07 09:17:26 +0000

    Minor fix

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/threatworx/twigs',
-    version='1.2.84',
+    version='1.2.85',
     zip_safe=False,
     entry_points={
         'console_scripts': [
