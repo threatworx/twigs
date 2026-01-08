03cb4092f740d895a3b10b0eb32537d3080162dd
Paresh Borkar <pareshborkar@gmail.com>
2026-01-08 10:00:04 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/threatworx/twigs',
-    version='1.2.85',
+    version='1.2.86',
     zip_safe=False,
     entry_points={
         'console_scripts': [
