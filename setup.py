8b8d7c3264ccc1c149eb6dd5141233f3c8f7dc07
Ketan Nilangekar <ketonne@yahoo.com>
2026-05-29 17:12:07 +0000

    Bump

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/threatworx/twigs',
-    version='1.2.97',
+    version='1.2.98',
     zip_safe=False,
     entry_points={
         'console_scripts': [
