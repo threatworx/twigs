f63bf7166e8dbfeb9ba914161cd27fc60800ef53
Paresh Borkar <pareshborkar@gmail.com>
2026-08-21 09:29:41 +0000

    Roll multiple misconfig isuses into single one. Upgrade to latest version of Lynis

--- twigs/host_benchmark/host_benchmark.py
+++ twigs/host_benchmark/host_benchmark.py
@@ -12,46 +12,60 @@ import traceback
 
 from twigs import utils
 
+# rdict contains recommendations from recommendations.json
+rdict = None
+
 # Note this error routine assumes that the file was read-only and hence could not be deleted
 def on_rm_error( func, path, exc_info):
     os.chmod( path, stat.S_IWRITE )
     os.unlink( path )
 
-def process_line(line, rating, asset_id):
-    bundled_recommendations_path = os.path.dirname(os.path.realpath(__file__)) + os.sep + 'recommendations.json'
-    rf = open(bundled_recommendations_path, 'r')
-    rdata = rf.read()
-    rf.close()
-    rdict = json.loads(rdata)
+def process_line(line, rating, asset_id, config_issues_dict, config_issues):
+    global rdict
     line_fields = line.split('|')
     category = line_fields[0]
     category_url = "https://cisofy.com/lynis/controls/" + category
     description = line_fields[1]
     suggestion = line_fields[2] if line_fields[2] != '-' else ''
     extra = line_fields[3] if line_fields[3] != '-' else ''
-    config_issue = { }
     twc_id_str = category + '-' + description
-    if len(suggestion) > 0:
-        twc_id_str = twc_id_str + '-' + suggestion
     twc_id_hash = hashlib.md5(twc_id_str.encode())
-    config_issue['twc_id'] = twc_id_hash.hexdigest()
+    twc_id_hash = twc_id_hash.hexdigest()
+    existing_ci  = config_issues_dict.get(twc_id_hash)
+    if existing_ci is not None:
+        # update details in existing issue
+        if len(suggestion) > 0:
+            existing_ci['details'] = existing_ci['details'] + '<br/>' + suggestion
+        return
+
+    # create new issue
+    config_issue = { }
+    config_issue['twc_id'] = twc_id_hash
     config_issue['asset_id'] = asset_id
     config_issue['twc_title'] = "[%s] %s" % (category, description)
     if rdict.get(category):
-        details = rdict[category] 
+        details = rdict[category]
     else:
-        details = suggestion + '\n\nFor additional information refer:\n' + category_url + '\n'
-        details = details.strip()
+        details = 'For additional information refer:\n' + category_url + '<br/>'
+    if len(suggestion) > 0:
+        details = details + '<b>Finding / Recommendation:</b><br/>' + suggestion
     config_issue['details'] = details
     config_issue['type'] = 'Host Benchmark'
     config_issue['rating'] = rating
     config_issue['object_id'] = ''
     config_issue['object_meta'] = ''
-    return config_issue
+    config_issues_dict[twc_id_hash] = config_issue
+    config_issues.append(config_issue)
 
 def process_report(report_file, asset_id):
     bundled_recommendations_path = os.path.dirname(os.path.realpath(__file__)) + os.sep + 'recommendations.json'
+    rf = open(bundled_recommendations_path, 'r')
+    rdata = rf.read()
+    rf.close()
+    global rdict
+    rdict = json.loads(rdata)
     config_issues = []
+    config_issues_dict = { }
     with open(report_file, 'r') as rpt_file:
         while True:
             line = rpt_file.readline()
@@ -59,11 +73,9 @@ def process_report(report_file, asset_id):
                 break
             line = line.strip()
             if line.startswith('warning[]='):
-                config_issue = process_line(line[10:], '4', asset_id)
-                config_issues.append(config_issue)
+                process_line(line[10:], '4', asset_id, config_issues_dict, config_issues)
             elif line.startswith('suggestion[]='):
-                config_issue = process_line(line[13:], '2', asset_id)
-                config_issues.append(config_issue)
+                process_line(line[13:], '2', asset_id, config_issues_dict, config_issues)
     return config_issues
 
 def run_host_benchmark(host, asset_id, args):
@@ -71,7 +83,7 @@ def run_host_benchmark(host, asset_id, args):
     try:
         logging.info("Running host benchmark for [%s]. This may take some time...", host['hostname'])
         args_encoding = args.encoding if args is not None else 'latin-1'
-        bundled_tar_path = os.path.dirname(os.path.realpath(__file__)) + os.sep + 'lynis-3.1.1.tar.gz'
+        bundled_tar_path = os.path.dirname(os.path.realpath(__file__)) + os.sep + 'lynis-3.1.7.tar.gz'
         local_temp_dir = tempfile.mkdtemp()
 
         # Untar tar to tmp directory
@@ -88,11 +100,11 @@ def run_host_benchmark(host, asset_id, args):
 
             remote_temp_dir = remote_temp_dir.strip()
             # SCP tar contents to remote box
-            utils.scp_put_file(host, local_temp_dir + os.sep + 'lynis-3.1.1', remote_temp_dir)
+            utils.scp_put_file(host, local_temp_dir + os.sep + 'lynis', remote_temp_dir)
             extract_tar_path = remote_temp_dir
 
         # Run host benchmark tool
-        cmdarr = ['cd ' + extract_tar_path + os.sep + 'lynis-3.1.1 && chmod 640 include/* && ./lynis audit system --quiet --logfile ../tw_lynis.log --report-file ../tw_lynis_report.dat && cd -']
+        cmdarr = ['cd ' + extract_tar_path + os.sep + 'lynis && chmod 640 include/* && ./lynis audit system --quiet --logfile ../tw_lynis.log --report-file ../tw_lynis_report.dat && cd -']
         utils.run_cmd_on_host(args, host, cmdarr)
 
         if host['remote'] == True:
